fbf658e2f52d2fd73f9adfd956d8e60d487529a5
Leandrocct <leandro.sonio1617@gmail.com>
2020-11-02 21:45:51 +0000

    First Commit

---
+++ src/barber_ca/Barber_Ca.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package barber_ca;
+
+/**
+ *
+ * @author User
+ */
+public class Barber_Ca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application losyougic here
+        System.out.println("Hello World");
+    }
+    
+}
