3a612e55a4bc706c7600d7a2ec1f745f96df02c4
Rafaelsousaa <rafael.current@gmail.com>
2020-12-09 18:28:20 +0000

    update in signup Panel

--- src/views/Frame.java
+++ src/views/Frame.java
@@ -1,33 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package views;
 
-
-
 import javax.swing.JFrame;
-import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.JPanel;
 
-/**
- *
- * @author User this is only for opening the window.
- */
-public class Frame  extends JFrame{
-    
-    
-    public Frame(int width, int height, JPanel panel){
-         
-       
-        this.setSize(width,height);
+public class Frame extends JFrame {
+
+    public Frame(int width, int height, JPanel panel) {
+        this.setSize(width, height);
         this.setVisible(true);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(3);
         this.setContentPane(panel);
-        
         this.repaint();
         this.validate();
     }
-    
-   }
+}
--- src/views/signupComponents/SignupPanel.java
+++ src/views/signupComponents/SignupPanel.java
@@ -7,122 +7,117 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-/**
- *
- * @author User
- */
 public class SignupPanel extends JPanel {
-
-   private JPanel headerPanel;
-    private JPanel midlePanel;
-    private JPanel emailPanel;
-    private JPanel fullNamePanel;
-    private JPanel phonePanel;
-    private JPanel passwordPanel;
-    private JPanel locationPanel;
-    private JPanel userTypePanel;
-    private JPanel registerButtonPanel;
+    private JPanel headersPanel = new JPanel();
+    private JPanel middlePanel = new JPanel();
+    private JPanel southPanel = new JPanel();
+    private JPanel emailPanel = new JPanel();
+    private JPanel phonePanel = new JPanel();
+    private JPanel passwordPanel = new JPanel();
+    private JPanel locationPanel = new JPanel();
+    private JPanel userTypePanel = new JPanel();
+    private JPanel fullNamePanel = new JPanel();
     private JComboBox<String> userTypeComboBox;
-    private JTextField emailTextFiled;
-    private JTextField localTextFiled;
-    private JTextField fullNameTextFiled;
-    private JTextField phoneTextFiled;
-    private JTextField passwordTextField;
-    private JButton registerButton;
+    private JTextField emailTextField;
+    private JTextField phoneTextField = new JTextField();
+    private JPasswordField passwordTextField = new JPasswordField();
+    private JTextField locationTextField;
+    private JTextField fullNameTextField = new JTextField();
+    private JButton signup = new JButton("Register");
+    private JButton backToLogin = new JButton("Back");
 
     public SignupPanel() {
-
-        this.headerPanel = new JPanel();
-        this.midlePanel = new JPanel();
-
-        locationPanel = new JPanel();
-        midlePanel = new JPanel();
-        headerPanel = new JPanel();
-        registerButtonPanel = new JPanel();
-        emailPanel = new JPanel();
-        fullNamePanel = new JPanel();
-        phonePanel = new JPanel();
-        passwordPanel = new JPanel();
-        userTypePanel = new JPanel();
-        emailTextFiled = new JTextField(10);
-        localTextFiled = new JTextField(10);
-        fullNameTextFiled = new JTextField(10);
-        phoneTextFiled = new JTextField(10);
-        passwordTextField = new JTextField(10);
-
-        emailPanel.setLayout(new GridLayout(1, 2));
-        locationPanel.setLayout(new GridLayout(1, 2));
-        userTypePanel.setLayout(new GridLayout(1, 2));
-        fullNamePanel.setLayout(new GridLayout(1, 2));
-        phonePanel.setLayout(new GridLayout(1, 2));
-        passwordPanel.setLayout(new GridLayout(1, 2));
-
-        userTypeComboBox = new JComboBox(new String[]{"costumer", "service provaid"});
-        locationPanel.setVisible(false);
-        JLabel emalLabel = new JLabel("Email");
+        this.emailPanel.setLayout(new GridLayout(1, 2));
+        this.locationPanel.setLayout(new GridLayout(1, 2));
+        this.userTypePanel.setLayout(new GridLayout(1, 2));
+        this.locationPanel.setVisible(false);
+        this.userTypeComboBox = new JComboBox(new String[]{"customer", "service provider"});
+        this.emailTextField = new JTextField();
+        this.locationTextField = new JTextField();
+        JLabel emailLabel = new JLabel("Email");
         JLabel userTypeLabel = new JLabel("User Type");
-        JLabel localtionLabel = new JLabel("Location");
-        JLabel phoneLabel = new JLabel("Phone");
-        JLabel fullNameLabel = new JLabel("Full Name");
-        JLabel passwordLabel = new JLabel("Password");
-        midlePanel.setLayout(new GridLayout(15, 1));
-
-        this.registerButton = new JButton("Register");
-        this.registerButtonPanel.add(registerButton);
-
-        phonePanel.add(phoneLabel);
-        phonePanel.add(phoneTextFiled);
-
-        fullNamePanel.add(fullNameLabel);
-        fullNamePanel.add(fullNameTextFiled);
-
-        passwordPanel.add(passwordLabel);
-        passwordPanel.add(passwordTextField);
-
-        emailPanel.add(emalLabel);
-        emailPanel.add(emailTextFiled);
-
-        locationPanel.add(localtionLabel);
-        locationPanel.add(localTextFiled);
-
-        userTypePanel.add(userTypeLabel);
-        userTypePanel.add(userTypeComboBox);
-
-        midlePanel.add(userTypePanel);
-        midlePanel.add(new JLabel());
-        midlePanel.add(fullNamePanel);
-        midlePanel.add(new JLabel());
-        midlePanel.add(emailPanel);
-        midlePanel.add(new JLabel());
-        midlePanel.add(phonePanel);
-        midlePanel.add(new JLabel());
-        midlePanel.add(passwordPanel);
-        midlePanel.add(new JLabel());
-        midlePanel.add(locationPanel);
-        midlePanel.add(new JLabel());
-        midlePanel.add(registerButtonPanel);
+        JLabel locationLabel = new JLabel("Location");
+        this.emailPanel.add(emailLabel);
+        this.emailPanel.add(this.emailTextField);
+        this.locationPanel.add(locationLabel);
+        this.locationPanel.add(this.locationTextField);
+        this.userTypePanel.add(userTypeLabel);
+        this.userTypePanel.add(this.userTypeComboBox);
+        this.middlePanel.add(this.userTypePanel);
+        this.middlePanel.add(new JLabel());
+        this.middlePanel.add(new JLabel());
+        this.middlePanel.add(new JLabel());
+        this.middlePanel.add(this.emailPanel);
+        this.middlePanel.add(this.fullNamePanel);
+        this.fullNamePanel.add(new JLabel("Full Name"));
+        this.fullNamePanel.add(this.fullNameTextField);
+        this.fullNamePanel.setLayout(new GridLayout(1, 2));
+        this.phonePanel.setLayout(new GridLayout(1, 2));
+        this.passwordPanel.setLayout(new GridLayout(1, 2));
+        this.phonePanel.add(new JLabel("Phone Number"));
+        this.phonePanel.add(this.phoneTextField);
+        this.passwordPanel.add(new JLabel("Password"));
+        this.passwordPanel.add(this.passwordTextField);
+        this.middlePanel.add(this.passwordPanel);
+        this.middlePanel.add(this.phonePanel);
+        this.middlePanel.add(this.locationPanel);
+        this.middlePanel.setLayout(new GridLayout(10, 1));
+        this.southPanel.add(new JLabel());
+        this.southPanel.add(this.backToLogin);
+        this.southPanel.add(new JLabel());
+        this.southPanel.add(this.signup);
+        this.southPanel.add(new JLabel());
+        this.southPanel.setLayout(new GridLayout(1, 5));
         this.setLayout(new BorderLayout());
-        this.add(headerPanel, BorderLayout.NORTH);
-        this.add(midlePanel, BorderLayout.CENTER);
-
-        JLabel signupLabel = new JLabel("SignupPage");
-        headerPanel.add(signupLabel);
-
+        this.add(this.headersPanel, "North");
+        this.add(this.middlePanel, "Center");
+        this.add(this.southPanel, "South");
+        JLabel signupLabel = new JLabel("Signup page");
+        this.headersPanel.add(signupLabel);
     }
 
-    public void changeLocation() {
-        String selectedUserType = userTypeComboBox.getItemAt(userTypeComboBox.getSelectedIndex());
-        if (selectedUserType.equals("costumer")) {
-            locationPanel.setVisible(false);
+    public void changeLocationState() {
+        String selectedUserType = (String)this.userTypeComboBox.getItemAt(this.userTypeComboBox.getSelectedIndex());
+        if (selectedUserType.equals("customer")) {
+            this.locationPanel.setVisible(false);
         } else {
-            locationPanel.setVisible(true);
+            this.locationPanel.setVisible(true);
         }
+
     }
 
-    public JComboBox<String> getUserTypeBox() {
-        return userTypeComboBox;
+    public JComboBox<String> getUserTypeComboBox() {
+        return this.userTypeComboBox;
     }
 
+    public String getUserType() {
+        return (String)this.userTypeComboBox.getSelectedItem();
+    }
+
+    public String getFullName() {
+        return this.fullNameTextField.getText();
+    }
+
+    public String getEmail() {
+        return this.emailTextField.getText();
+    }
+
+    public String getPhone() {
+        return this.phoneTextField.getText();
+    }
+
+    public String getPassword() {
+        return new String(this.passwordTextField.getPassword());
+    }
+
+    public String getLocationText() {
+        return this.locationTextField.getText();
+    }
+
+    public JButton getSignup() {
+        return this.signup;
+    }
 }
